02035145e068769f98f78ab4503e31e38adbecac
NikolasEagle <eagle.dev.stack@gmail.com>
2025-02-24 17:43:02 +0400

    Исправление разметки

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,12 @@
-import SearchField from "@/components/SearchField/SearchField";
 import styles from "./home.module.scss";
+import SearchField from "@/components/SearchField/SearchField";
+import Contents from "@/components/Contents/Contents";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <SearchField />
+      <Contents />
     </div>
   );
 }
---
+++ src/components/Contents/Contents.tsx
@@ -0,0 +1,10 @@
+import styles from "./Contents.module.scss";
+
+export default function Contents() {
+  return (
+    <div className={styles.Contents}>
+      {/*<ResultsInfo/>
+        <BookCards/>*/}
+    </div>
+  );
+}
