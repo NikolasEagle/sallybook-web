c3b205101e433e3c8dcbf2b38587173e28036ea4
Nickolas <eagle.dev.stack@gmail.com>
2025-06-10 14:26:16 +0400

    Очистка next.config от старых данных

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: `http://localhost:8080/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
