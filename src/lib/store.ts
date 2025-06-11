d6e232e488317cca5757dab895e63f273ba81c03
Nickolas <eagle.dev.stack@gmail.com>
2025-06-11 11:24:12 +0400

    Добавление chaptersReducer в configureStore

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./features/books/booksSlice";
+import chaptersReducer from "./features/chapters/chaptersSlice";
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
+    chapters: chaptersReducer,
   },
 });
 
