f9065edccdf5c933f38fed083a88faffeda63ee4
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 22:27:22 +0400

    Добавление в редьюсера страницы авторизации в redux хранилище

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./features/books/booksSlice";
 import chaptersReducer from "./features/chapters/chaptersSlice";
 import registerReducer from "./features/register/registerSlice";
+import loginReducer from "./features/login/loginSlice";
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     chapters: chaptersReducer,
     register: registerReducer,
+    login: loginReducer,
   },
 });
 
