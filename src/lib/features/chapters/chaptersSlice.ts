6bc096a1cad1ee788c08c2cebf185997c06c96f7
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 11:49:35 +0400

    Добавление состояния разделов приложения и их интерфейсов данных

--- src/lib/features/chapters/chaptersSlice.ts
+++ src/lib/features/chapters/chaptersSlice.ts
@@ -4,10 +4,27 @@ const initialState = {
   currentChapter: localStorage.getItem("currentChapter")
     ? JSON.parse(localStorage.getItem("currentChapter"))
     : null,
+  chapters: localStorage.getItem("chapters")
+    ? JSON.parse(localStorage.getItem("chapters"))
+    : null,
 };
 
+export interface Chapters {
+  icon: string;
+
+  iconActive: string;
+
+  name: string;
+
+  active: boolean;
+}
+
 export interface StateChapters {
-  currentChapter: string;
+  chapters: {
+    chapters: Chapters;
+
+    currentChapter: string;
+  };
 }
 
 const chaptersSlice = createSlice({
