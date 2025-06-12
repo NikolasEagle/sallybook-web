e9c29080d92941e36f78c4dc5baec0f9cafd1ca6
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 19:50:16 +0400

    Добавление базовой структуры функции регистрации

--- src/components/FormRegister/FormRegister.tsx
+++ src/components/FormRegister/FormRegister.tsx
@@ -1,18 +1,67 @@
+"use client";
+
 import Form from "next/form";
 
 import styles from "./FormRegister.module.scss";
 import FormHeader from "../FormHeader/FormHeader";
+import { FormEvent, useState } from "react";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 export default function FormRegister() {
+  const [message, setMessage] = useState<string>("");
+
+  const url = `http://${process.env.NEXT_PUBLIC_HOST_SERVER_AUTH}:${process.env.NEXT_PUBLIC_PORT_SERVER_AUTH}/register`;
+
+  async function register(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const password = formData.get("password");
+
+    const repeatPassword = formData.get("repeat_password");
+
+    try {
+      if (password === repeatPassword) {
+        const response = await fetch(url, {
+          method: "POST",
+          body: formData,
+        });
+        if (response.status === 201) {
+          formData.get
+        } else if (
+          response.status === 401 ||
+          response.status === 409 ||
+          response.status === 500
+        ) {
+          throw new Error(await response.text());
+        }
+      } else {
+        throw new Error("Passwords don't match");
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setMessage(error.message);
+      }
+    }
+  }
+
   return (
     <Form
-      action={`http://${process.env.NEXT_PUBLIC_HOST_SERVER_AUTH}:${process.env.NEXT_PUBLIC_PORT_SERVER_AUTH}/register`}
+      action={""}
+      onSubmit={(event) => register(event)}
       className={styles.FormRegister}
     >
       <FormHeader name={"Регистрация"} />
       <input required type="text" name="second_name" placeholder="Фамилия" />
       <input required type="text" name="first_name" placeholder="Имя" />
-      <input required type="text" name="email" placeholder="Email" />
+      <input
+        pattern="^\S+@\S+\.\S+$"
+        required
+        type="text"
+        name="email"
+        placeholder="Email"
+      />
       <input
         required
         type="password"
@@ -26,6 +75,7 @@ export default function FormRegister() {
         placeholder="Повторите пароль"
       />
       <button type="submit">Регистрация</button>
+      <ErrorMessage message={message} />
     </Form>
   );
 }
