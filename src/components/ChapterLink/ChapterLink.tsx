f96dccafd13930b9cf528babfcce9ca9e8e52232
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 13:20:08 +0400

    Реализация функции переключения между разделами на нижней панели

--- src/components/ChapterLink/ChapterLink.tsx
+++ src/components/ChapterLink/ChapterLink.tsx
@@ -4,24 +4,38 @@ import { useRouter } from "next/navigation";
 
 import { useDispatch } from "react-redux";
 
+import { SET_LOADING } from "@/lib/features/chapters/chaptersSlice";
+
 interface Props {
   icon: string;
+  iconActive: string;
   name: string;
   href: string;
   active: boolean;
 }
 
-export default function ChapterLink({ icon, name, href, active }: Props) {
+export default function ChapterLink({
+  icon,
+  iconActive,
+  name,
+  href,
+  active,
+}: Props) {
   const router = useRouter();
 
   const dispatch = useDispatch();
 
-  function goToChapter() {}
+  function goToChapter() {
+    dispatch(SET_LOADING(true));
+    router.push(href);
+  }
 
   return (
     <div onClick={goToChapter} tabIndex={0} className={styles.ChapterLink}>
-      <img src={icon} alt={name} />
-      <p>{name}</p>
+      <img src={active ? iconActive : icon} alt={name} />
+      <p style={active ? { fontWeight: "bold" } : { fontWeight: "normal" }}>
+        {name}
+      </p>
     </div>
   );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,33 +6,14 @@ import ChapterLink from "../ChapterLink/ChapterLink";
 import { useSearchParams, usePathname } from "next/navigation";
 
 import { useDispatch, useSelector } from "react-redux";
-import { SET_CURRENT_CHAPTER } from "@/lib/features/chapters/chaptersSlice";
+
+import {
+  SET_CHAPTERS,
+  SET_CURRENT_CHAPTER,
+} from "@/lib/features/chapters/chaptersSlice";
+
 import { StateChapters } from "@/lib/features/chapters/chaptersSlice";
-import { useEffect, useState } from "react";
-
-const chapters = [
-  {
-    icon: "/home.svg",
-
-    name: "Главная",
-
-    href: "/home",
-  },
-  {
-    icon: "/profile.svg",
-
-    name: "Профиль",
-
-    href: "/profile",
-  },
-  {
-    icon: "/settings.svg",
-
-    name: "Настройки",
-
-    href: "/settings",
-  },
-];
+import { useEffect } from "react";
 
 export default function Footer() {
   const searchParams = useSearchParams();
@@ -43,15 +24,34 @@ export default function Footer() {
 
   const dispatch = useDispatch();
 
-  if (/^\/home/.test(pathName)) {
-    dispatch(SET_CURRENT_CHAPTER("home"));
-  } else if (/^\/profile/.test(pathName)) {
-    dispatch(SET_CURRENT_CHAPTER("profile"));
-  } else if (/^\/settings/.test(pathName)) {
-    dispatch(SET_CURRENT_CHAPTER("settings"));
-  }
+  const chapters = useSelector(
+    (state: StateChapters) => state.chapters.chapters
+  );
 
-  const [active, setActive] = useState<boolean>();
+  useEffect(() => {
+    if (/^\/home/.test(pathName)) {
+      dispatch(SET_CURRENT_CHAPTER("home"));
+    } else if (/^\/profile/.test(pathName)) {
+      dispatch(SET_CURRENT_CHAPTER("profile"));
+    } else if (/^\/settings/.test(pathName)) {
+      dispatch(SET_CURRENT_CHAPTER("settings"));
+    }
+
+    dispatch(
+      SET_CHAPTERS(
+        chapters.map((chapter) => {
+          const newChapter = { ...chapter };
+
+          if (chapter.href === pathName) {
+            newChapter.active = true;
+          } else {
+            newChapter.active = false;
+          }
+          return newChapter;
+        })
+      )
+    );
+  }, [pathName]);
 
   return (
     pathName !== "/reader" && (
@@ -64,8 +64,10 @@ export default function Footer() {
         {chapters.map((chapter) => (
           <ChapterLink
             icon={chapter.icon}
+            iconActive={chapter.iconActive}
             name={chapter.name}
             href={chapter.href}
+            active={chapter.active}
           />
         ))}
       </footer>
