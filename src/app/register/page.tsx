567a1d8987c010d787d704c552a9fc8fc5b84796
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 15:58:49 +0400

    Добавление базовых стилей для формы регистрации

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,11 @@
 import styles from "./register.module.scss";
 
+import FormRegister from "@/components/FormRegister/FormRegister";
+
 export default function RegisterPage() {
-  return <div className={styles.register}></div>;
+  return (
+    <div className={styles.register}>
+      <FormRegister />
+    </div>
+  );
 }
