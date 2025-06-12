ef15abb97a13342d59830a829fe004008b7b78fe
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 15:48:10 +0400

    Добавление полей ввода и кнопки

--- src/components/Form/FormRegister.tsx
+++ src/components/Form/FormRegister.tsx
@@ -1,13 +1,20 @@
 import Form from "next/form";
 
-interface Props {
-  kind: string;
-}
-
-export default function FormRegister({ kind }: Props) {
+export default function FormRegister() {
   return (
     <Form
       action={`http://${process.env.NEXT_PUBLIC_HOST_SERVER_AUTH}:${process.env.NEXT_PUBLIC_PORT_SERVER_AUTH}/register`}
-    ></Form>
+    >
+      <input type="text" name="second_name" placeholder="Фамилия" />
+      <input type="text" name="first_name" placeholder="Имя" />
+      <input type="text" name="email" placeholder="Email" />
+      <input type="password" name="password" placeholder="Придумайте пароль" />
+      <input
+        type="password"
+        name="repeat_password"
+        placeholder="Повторите пароль"
+      />
+      <button type="submit">Регистрация</button>
+    </Form>
   );
 }
