fac32c2da86f92d431ab14d3147bdd6e66ce6411
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 21:14:17 +0400

    Добавление изменения компонента загрузки в зависимости от текущего маршрута

--- src/components/Download/Download.tsx
+++ src/components/Download/Download.tsx
@@ -3,9 +3,25 @@ import styles from "./Download.module.scss";
 import { useSelector } from "react-redux";
 
 import { StateBooks } from "@/lib/features/books/booksSlice";
+import { usePathname } from "next/navigation";
+import { StateRegister } from "@/lib/features/register/registerSlice";
 
 export default function Download() {
-  const isLoading = useSelector((state: StateBooks) => state.books.isLoading);
+  const pathName = usePathname();
+
+  const regex = new RegExp(`^\/home|bookPage`);
+
+  const regexRegister = new RegExp(`^\/register`);
+
+  let isLoading: boolean;
+
+  if (regex.test(pathName)) {
+    isLoading = useSelector((state: StateBooks) => state.books.isLoading);
+  } else if (regexRegister.test(pathName)) {
+    isLoading = useSelector((state: StateRegister) => state.register.isLoading);
+  } else {
+    isLoading = false;
+  }
 
   return (
     <div
