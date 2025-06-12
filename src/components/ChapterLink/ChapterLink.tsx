e8db234372a72d9e6239d84c9102b8a6575a444f
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 12:04:19 +0400

    Создание базовой структуры компонента для реализации навигации по разделам

--- src/components/ChapterLink/ChapterLink.tsx
+++ src/components/ChapterLink/ChapterLink.tsx
@@ -1,17 +1,27 @@
-import Link from "next/link";
 import styles from "./ChapterLink.module.scss";
+
+import { useRouter } from "next/navigation";
+
+import { useDispatch } from "react-redux";
 
 interface Props {
   icon: string;
   name: string;
   href: string;
+  active: boolean;
 }
 
-export default function ChapterLink({ icon, name, href }: Props) {
+export default function ChapterLink({ icon, name, href, active }: Props) {
+  const router = useRouter();
+
+  const dispatch = useDispatch();
+
+  function goToChapter() {}
+
   return (
-    <Link href={href} autoFocus tabIndex={0} className={styles.ChapterLink}>
+    <div onClick={goToChapter} tabIndex={0} className={styles.ChapterLink}>
       <img src={icon} alt={name} />
       <p>{name}</p>
-    </Link>
+    </div>
   );
 }
