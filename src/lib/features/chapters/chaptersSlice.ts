b3a00a332f05016b351ab10ac89455b3ef4fc9c4
Nickolas <eagle.dev.stack@gmail.com>
2025-06-12 11:52:51 +0400

    Создание редьюсера записи разделов в хранилище redux

--- src/lib/features/chapters/chaptersSlice.ts
+++ src/lib/features/chapters/chaptersSlice.ts
@@ -35,8 +35,12 @@ const chaptersSlice = createSlice({
       state.currentChapter = action.payload;
       localStorage.setItem("currentChapter", JSON.stringify(action.payload));
     },
+    SET_CHAPTERS: (state, action) => {
+      state.chapters = action.payload;
+      localStorage.setItem("chapters", JSON.stringify(action.payload));
+    },
   },
 });
 
-export const { SET_CURRENT_CHAPTER } = chaptersSlice.actions;
+export const { SET_CURRENT_CHAPTER, SET_CHAPTERS } = chaptersSlice.actions;
 export default chaptersSlice.reducer;
