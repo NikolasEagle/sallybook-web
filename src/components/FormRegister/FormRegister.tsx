36d82d42335d2e40d40d76228a85c1ed64c819dd
Nikolas <eagle.dev.stack@gmail.com>
2025-06-14 01:48:18 +0400

    Изменение кнопки перехода на страницу авторизации

--- src/components/FormRegister/FormRegister.tsx
+++ src/components/FormRegister/FormRegister.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/lib/features/register/registerSlice";
 import { SuccessMessage } from "../SuccessMessage/SuccessMessage";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function FormRegister() {
   const router = useRouter();
@@ -73,60 +74,61 @@ export default function FormRegister() {
     }
   }
 
-  function goToLoginPage() {
-    router.push("/login");
-  }
-
   return isLoading ? (
     <Download />
   ) : success ? (
     <SuccessMessage email={email} />
   ) : (
-    <Form
-      action={""}
-      onSubmit={(event) => register(event)}
-      className={styles.FormRegister}
-    >
-      <FormHeader name={"Регистрация"} />
-      <input
-        autoComplete="off"
-        required
-        type="text"
-        name="second_name"
-        placeholder="Фамилия"
-      />
-      <input
-        autoComplete="off"
-        required
-        type="text"
-        name="first_name"
-        placeholder="Имя"
-      />
-      <input
-        autoComplete="off"
-        pattern="^\S+@\S+\.\S+$"
-        required
-        type="text"
-        name="email"
-        placeholder="Email"
-      />
-      <input
-        autoComplete="off"
-        required
-        type="password"
-        name="password"
-        placeholder="Придумайте пароль"
-      />
-      <input
-        autoComplete="off"
-        required
-        type="password"
-        name="repeat_password"
-        placeholder="Повторите пароль"
-      />
-      <button type="submit">Регистрация</button>
-      <button onClick={goToLoginPage}>Назад</button>
-      <ErrorMessage message={message} />
-    </Form>
+    <div className={styles.FormRegister}>
+      <Form
+        action={""}
+        onSubmit={(event) => register(event)}
+        className={styles.form}
+      >
+        <FormHeader name={"Регистрация"} />
+        <input
+          autoComplete="off"
+          required
+          type="text"
+          name="second_name"
+          placeholder="Фамилия"
+        />
+        <input
+          autoComplete="off"
+          required
+          type="text"
+          name="first_name"
+          placeholder="Имя"
+        />
+        <input
+          autoComplete="off"
+          pattern="^\S+@\S+\.\S+$"
+          required
+          type="text"
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          autoComplete="off"
+          required
+          type="password"
+          name="password"
+          placeholder="Придумайте пароль"
+        />
+        <input
+          autoComplete="off"
+          required
+          type="password"
+          name="repeat_password"
+          placeholder="Повторите пароль"
+        />
+        <button type="submit">Регистрация</button>
+
+        <ErrorMessage message={message} />
+      </Form>
+      <Link className={styles.linkToLogin} href={"/login"}>
+        На страницу входа
+      </Link>
+    </div>
   );
 }
