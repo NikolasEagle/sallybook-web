ce5865d83e3fdf220a0d69f50908f8f31d3a5be9
Nikolas <eagle.dev.stack@gmail.com>
2025-06-14 16:26:16 +0400

    Реализация запроса данных через сессионые куки

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,11 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./UserInfo.module.scss";
 
 export default function UserInfo() {
+  const [firstName, setFirstName] = useState<string>("");
+
+  const [secondName, setSecondName] = useState<string>("");
+
+  async function getUserInfo() {
+    const url = `http://${process.env.NEXT_PUBLIC_HOST_SERVER_AUTH}:${process.env.NEXT_PUBLIC_PORT_SERVER_AUTH}/check_auth`;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        credentials: "include",
+      });
+      if (response.status === 200) {
+        const json = await response.json();
+
+        const data = JSON.parse(json);
+
+        const { second_name, first_name } = data;
+
+        setSecondName(second_name);
+        setFirstName(first_name);
+      }
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  });
+
   return (
     <div className={styles.UserInfo}>
       <img className={styles.avatar} src="/avatar.png" alt="avatar" />
-      <h2></h2>
-      <h2></h2>
+      <h2>{secondName}</h2>
+      <h2>{firstName}</h2>
       <button className={styles.logout}>Выйти из аккаунта</button>
     </div>
   );
