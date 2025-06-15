e9bc3c5185ce18598a96a7ba1eb8427fdf8ba5eb
Nickolas <eagle.dev.stack@gmail.com>
2025-06-15 14:36:59 +0400

    Изменение тега с Image на img

--- src/components/BookImage/BookImage.tsx
+++ src/components/BookImage/BookImage.tsx
@@ -3,7 +3,6 @@ import styles from "./BookImage.module.scss";
 import { useSelector } from "react-redux";
 
 import { StateBooks } from "@/lib/features/books/booksSlice";
-import Image from "next/image";
 
 export default function BookImage() {
   const selectedBook = useSelector(
@@ -11,10 +10,8 @@ export default function BookImage() {
   );
 
   return selectedBook ? (
-    <Image
+    <img
       className={styles.BookImage}
-      width={"200"}
-      height={"200"}
       src={selectedBook.cover || "/no-image.png"}
       alt="BookImage"
     />
